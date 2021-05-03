e573ceff316f1e8639970979d9294797ee775994
dionisiusst2 <dionisiusst2@gmail.com>
2021-05-03 16:56:24 +0700

    Add SortAlgorithm interface

---
+++ src/algorithm/SortAlgorithm.java
@@ -0,0 +1,8 @@
+package algorithm;
+import java.util.ArrayList;
+
+public interface SortAlgorithm {
+	public static final int ASCENDING = 1;
+	public static final int DESCENDING = 2;
+	public void sort(ArrayList<Integer> list, int type);
+}
