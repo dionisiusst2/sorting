30488c0efd1ec82467495b0822bd743d02fe7d73
dionisiusst2 <dionisiusst2@gmail.com>
2021-05-04 16:36:00 +0700

    Update BubbleSort, remove mode

--- src/algorithm/BubbleSort.java
+++ src/algorithm/BubbleSort.java
@@ -5,29 +5,17 @@ import java.util.ArrayList;
 public class BubbleSort implements SortAlgorithm{
 
 	@Override
-	public void sort(ArrayList<Integer> list, int mode) {
-		bubbleSort(list, mode);
+	public void sort(ArrayList<Integer> list) {
+		bubbleSort(list);
 	}
 	
-	private void bubbleSort(ArrayList<Integer> list, int mode) {
-		if(mode == ASCENDING) {
-			for(int i=0; i<list.size(); i++) {
-				for(int j=0; j<list.size()-i-1; j++) {
-					if(list.get(i) < list.get(j)) {
-						int temp = list.get(i);
-						list.set(i, list.get(j));
-						list.set(j, temp);
-					}
-				}
-			}
-		} else {
-			for(int i=0; i<list.size(); i++) {
-				for(int j=0; j<list.size()-i-1; j++) {
-					if(list.get(i) > list.get(j)) {
-						int temp = list.get(i);
-						list.set(i, list.get(j));
-						list.set(j, temp);
-					}
+	private void bubbleSort(ArrayList<Integer> list) {
+		for(int i=0; i<list.size(); i++) {
+			for(int j=0; j<list.size()-i-1; j++) {
+				if(list.get(i) < list.get(j)) {
+					int temp = list.get(i);
+					list.set(i, list.get(j));
+					list.set(j, temp);
 				}
 			}
 		}
