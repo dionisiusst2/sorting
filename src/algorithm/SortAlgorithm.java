f4e9ace83bfa8ddf76b0e7de13e6d82b476d93a0
dionisiusst2 <dionisiusst2@gmail.com>
2021-05-04 16:36:44 +0700

    Update SortAlgorithm interface, remove mode

--- src/algorithm/SortAlgorithm.java
+++ src/algorithm/SortAlgorithm.java
@@ -2,7 +2,5 @@ package algorithm;
 import java.util.ArrayList;
 
 public interface SortAlgorithm {
-	public static final int ASCENDING = 1;
-	public static final int DESCENDING = 2;
-	public void sort(ArrayList<Integer> list, int mode);
+	public void sort(ArrayList<Integer> list);
 }
