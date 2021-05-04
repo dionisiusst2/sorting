14248f97996410d78763769c00702c28a3614907
dionisiusst2 <dionisiusst2@gmail.com>
2021-05-04 15:33:32 +0700

    Update SortAlgorithm interface

--- src/algorithm/SortAlgorithm.java
+++ src/algorithm/SortAlgorithm.java
@@ -4,5 +4,5 @@ import java.util.ArrayList;
 public interface SortAlgorithm {
 	public static final int ASCENDING = 1;
 	public static final int DESCENDING = 2;
-	public void sort(ArrayList<Integer> list, int type);
+	public void sort(ArrayList<Integer> list, int mode);
 }
